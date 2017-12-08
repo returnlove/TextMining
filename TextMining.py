f2b728d4fc4e63bba9f2c4520cc1e4ce4f23ad6f
returnlove <iamsainath2@gmail.com>
2017-12-08 12:38:46 +0530

    format all reviews and create corpus

--- TextMining.py
+++ TextMining.py
@@ -10,24 +10,32 @@ from nltk.stem.porter import PorterStemmer
 
 
 dataset = pd.read_csv("Restaurant_Reviews.tsv", sep = "\t")
+corpus = []
 
-#select only letters
-review = re.sub('[^a-zA-Z]', ' ', dataset["Review"][0])
+for i in range(len(dataset)):
+    #select only letters
+    review = re.sub('[^a-zA-Z]', ' ', dataset["Review"][i])
 
-#convert letters to lower case
-review = review.lower()
+    #convert letters to lower case
+    review = review.lower()
+    
+    #plit the sent to words
+    review = review.split()
+    
+    ps = PorterStemmer()
+    
+    #review = [word for word in review if word not in set(stopwords.words('english'))]
+    
+    #remove stop words and stem
+    #stemming - considering only root of the word not different versions of the same word - to avoid too much of sparsity
+    
+    review = [ps.stem(word) for word in review if word not in set(stopwords.words('english'))]
+    
+    # make these words back to sent
+    
+    review = " ".join(review)
+    corpus.append(review)
 
-#plit the sent to words
-review = review.split()
-
-ps = PorterStemmer()
-
-#review = [word for word in review if word not in set(stopwords.words('english'))]
-
-#remove stop words and stem
-#stemming - considering only root of the word not different versions of the same word - to avoid too much of sparsity
-
-review = [ps.stem(word) for word in review if word not in set(stopwords.words('english'))]
 
 
 
