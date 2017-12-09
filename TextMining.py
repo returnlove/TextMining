dae200ba07054f9b32b7089c22e8d4a2a3d3e4bb
returnlove <iamsainath2@gmail.com>
2017-12-09 15:11:45 +0530

    bag of words

--- TextMining.py
+++ TextMining.py
@@ -6,7 +6,7 @@ import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 dataset = pd.read_csv("Restaurant_Reviews.tsv", sep = "\t")
@@ -15,26 +15,45 @@ corpus = []
 for i in range(len(dataset)):
     #select only letters
     review = re.sub('[^a-zA-Z]', ' ', dataset["Review"][i])
-
     #convert letters to lower case
-    review = review.lower()
-    
+    review = review.lower()    
     #plit the sent to words
-    review = review.split()
-    
-    ps = PorterStemmer()
-    
-    #review = [word for word in review if word not in set(stopwords.words('english'))]
-    
+    review = review.split()    
+    ps = PorterStemmer()    
+    #review = [word for word in review if word not in set(stopwords.words('english'))]    
     #remove stop words and stem
-    #stemming - considering only root of the word not different versions of the same word - to avoid too much of sparsity
-    
-    review = [ps.stem(word) for word in review if word not in set(stopwords.words('english'))]
-    
-    # make these words back to sent
-    
+    #stemming - considering only root of the word not different versions of the same word - to avoid too much of sparsity    
+    review = [ps.stem(word) for word in review if word not in set(stopwords.words('english'))]    
+    # make these words back to sent    
     review = " ".join(review)
     corpus.append(review)
+
+# creating bag of words model
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
+
+
+
 
 
 
